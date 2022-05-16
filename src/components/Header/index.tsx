53f7d8ef0aefed722f1cd1fcea609f2f82a13bbd
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-05-16 00:16:46 -0300

    add modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,17 @@
 import logo from '../../assets/images/logo.svg';
 import { Button, Container, Content, Image } from './styles';
-export function Header() {
+
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header(props: HeaderProps) {
     return (
         <Container>
             <Content>
                 <Image src={logo} alt="dt money" />
-                <Button type="button">
+                <Button type="button"
+                    onClick={props.onOpenNewTransactionModal}>
                     Nova transação
                 </Button>
             </Content>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import { Header } from "../../components/Header";
 import Dashboard from "../../components/Dashboard";
+import { NewTransactionModal } from "../../components/NewTransactionModal";
 
 export function Home() {
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true)
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false)
+    }
+
     return (
         <main>
-            <Header />
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
             <Dashboard />
+            <NewTransactionModal
+                isOpen={isNewTransactionModalOpen}
+                onRequestClose={handleCloseNewTransactionModal}
+            />
         </main>
     )
 }
